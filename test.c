b3aafab88a0514a76d2d8223b0d00adbe382f40f
Mitesh Kanjariya <mitesh@arrcus.com>
2020-10-01 19:50:26 +0000

    added new func

--- test.c
+++ test.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void func2 ()
+{
+  return;
+}
+
 void
 func (void)
 {
   int i = 0;
 
-  for (i = 0; i < 5; i++)
+  for (i = 0; i < 10; i++)
     printf("Hello World !!!\n");
   return;
 }
