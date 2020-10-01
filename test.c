d79d0efc0307d3ffef8be3ea366a3978f5024a9c
Mitesh Kanjariya <mitesh@arrcus.com>
2020-10-01 19:28:40 +0000

    added a new func for hello world
    Moved the code for printing helloworld to a new func

--- test.c
+++ test.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int
-main ()
+void
+func (void)
 {
   int i = 0;
 
-  for(i = 0; i < 5; i++)
-    printf("Hello world!!\n");
+  for (i = 0; i < 5; i++)
+    printf("Hello World !!!\n");
+  return;
+}
 
+int
+main ()
+{
+  func();
   return 0;
 }
